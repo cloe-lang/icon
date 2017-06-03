ddeb7627e08ff7298e2ed8b3c11f135f114c731d
Yota Toyama <raviqqe@gmail.com>
2017-06-04 06:01:04 +0900

    Apply linear gradient to shadow

--- icon.rb
+++ icon.rb
@@ -16,9 +16,16 @@ svg = xml do
   height = 180
 
   svg xmlns: 'http://www.w3.org/2000/svg', width: width, height: height do
-    path d: 'M 55 170 h 30 l 75 -50 l 5 -40 Z', fill: grey
-    path d: 'M 100 180 h 40 l 10 -110 h -40 Z', fill: grey
-    path d: 'M 0 180 h 40 l 200 -130 h -40 Z', fill: grey
+    defs do
+      linearGradient id: 'shadow', x1: '0%', y1: '100%', x2: '0%', y2: '0%' do
+        stop offset: '0%', 'stop-color' => grey
+        stop offset: '100%', 'stop-color' => 'white'
+      end
+    end
+
+    path d: 'M 55 170 h 30 l 75 -50 l 5 -40 Z', fill: 'url(#shadow)'
+    path d: 'M 100 180 h 40 l 10 -110 h -40 Z', fill: 'url(#shadow)'
+    path d: 'M 0 180 h 40 l 200 -130 h -40 Z', fill: 'url(#shadow)'
 
     path(
       d: 'M 55 170 h 30 l 35 -60 l -20 -40 l 30 -60 h -30 l -30 60 l 20 40 Z',
