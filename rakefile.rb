406d259943f2f4616198f5cc8b9365db18d70809
Yota Toyama <raviqqe@gmail.com>
2017-06-05 05:20:50 +0900

    Generate square icon

--- rakefile.rb
+++ rakefile.rb
@@ -3,6 +3,8 @@ dpi = 96 * 4
 task :default do
   sh './icon.rb > icon.svg'
   sh "inkscape -d #{dpi} --export-area-drawing --export-png icon.png icon.svg"
+  sh %W[inkscape -d #{dpi} --export-area -15:0:155:170 --export-png
+        square.png icon.svg].join ' '
 
   sh './icon.rb --landscape --shadow > landscape.svg'
   sh %W[inkscape -d #{dpi} --export-area -400:-40:540:210 --export-png
