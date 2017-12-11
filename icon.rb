14822fef944c0270a74e11f0587712c1b3c7307c
Yota Toyama <raviqqe@gmail.com>
2017-12-12 05:18:15 +0900

    Renew logo

--- icon.rb
+++ icon.rb
@@ -1,57 +1,14 @@
 #!/usr/bin/env ruby
 
-require 'chroma'
-require 'docopt'
 require 'xml-dsl'
 
-args = Docopt.docopt "
-Icon generator
-
-Usage:
-  #{__FILE__} [--shadow]
-"
-
-red, yellow = %w[#8c201d #9f7d35].map do |rgb|
-  rgb.paint.brighten(20).saturate(40).to_hex
-end
+GREEN = '#004225'.freeze
+YELLOW = '#F0C420'.freeze
 
 puts(xml do
-  svg xmlns: 'http://www.w3.org/2000/svg',
-      width: (args['--shadow'] ? 240 : 140),
-      height: 170 do
-    if args['--shadow']
-      defs do
-        linearGradient id: 'shadowColor',
-                       x1: '0%', y1: '100%',
-                       x2: '0%', y2: '0%' do
-          stop offset: '0%',
-               'stop-color' => 'black',
-               'stop-opacity' => '30%'
-          stop offset: '85%',
-               'stop-color' => 'black',
-               'stop-opacity' => '0%'
-        end
-
-        clipPath id: 'shadowPath' do
-          path d: 'M 55 160 h 30 l 75 -50 l 5 -40 Z'
-          path d: 'M 100 170 h 40 l 10 -110 h -40 Z'
-          path d: 'M 0 170 h 40 l 200 -130 h -40 Z'
-        end
-      end
-
-      rect width: '100%', height: '100%',
-           fill: 'url(#shadowColor)',
-           'clip-path' => 'url(#shadowPath)'
-    end
-
-    path(
-      d: 'M 55 160 h 30 l 35 -60 l -20 -40 l 30 -60 h -30 l -30 60 l 20 40 Z',
-      fill: yellow
-    )
-
-    path(
-      d: 'M 20 10 h 40 l 80 160 h -40 l -30 -60 l -30 60 h -40 l 50 -100 Z',
-      fill: red
-    )
+  svg xmlns: 'http://www.w3.org/2000/svg', width: 200, height: 320 do
+    rect width: '100%', height: '100%', fill: 'white'
+    polygon points: '0,0 200,0 0,320', fill: GREEN
+    polygon points: '75,320 200,120 200,320', fill: YELLOW
   end
 end)
--- rakefile.rb
+++ rakefile.rb
@@ -3,10 +3,6 @@ dpi = 96 * 4
 task :default do
   sh './icon.rb > icon.svg'
   sh "inkscape -d #{dpi} --export-area-drawing --export-png icon.png icon.svg"
-  sh %W[inkscape -d #{dpi} --export-area -15:0:155:170 --export-png
+  sh %W[inkscape -d #{dpi} --export-area -60:0:260:320 --export-png
         square.png icon.svg].join ' '
-
-  sh './icon.rb --shadow > shadowed.svg'
-  sh %w[inkscape -d 96 --export-area -100:-20:240:190 --export-png
-        shadowed.png shadowed.svg].join ' '
 end
