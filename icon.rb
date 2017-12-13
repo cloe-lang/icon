3f7aa9d75737ee01034e15edb2469b4a6df6f50d
Yota Toyama <raviqqe@gmail.com>
2017-12-14 02:00:45 +0900

    Fix thin transparent lines

--- icon.rb
+++ icon.rb
@@ -7,7 +7,7 @@ SECONDARY = 'tomato'.freeze
 
 puts(xml do
   svg xmlns: 'http://www.w3.org/2000/svg', width: 160, height: 160 do
-    path d: 'M 0 0 h 160 l -30 30 h -100 v 100 h 100 l 30 30 h -160',
+    path d: 'M 0 0 h 160 l -80 80 l 80 80 h -160',
          fill: PRIMARY
     rect x: 30, y: 30, width: 100, height: 100, fill: SECONDARY
     rect x: 60, y: 60, width: 40, height: 40, fill: PRIMARY
