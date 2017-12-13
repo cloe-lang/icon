d376da6d3c207003a73a1e88e1f02eb9c9cf11df
Yota Toyama <raviqqe@gmail.com>
2017-12-13 14:15:46 +0900

    Generate spaced image

--- rakefile.rb
+++ rakefile.rb
@@ -3,4 +3,6 @@ dpi = 96
 task :default do
   sh './icon.rb > icon.svg'
   sh "inkscape -d #{dpi} --export-area-drawing --export-png icon.png icon.svg"
+  sh %W[inkscape -d #{dpi} --export-area -50:-50:210:210 --export-png
+        spaced.png icon.svg].join ' '
 end
