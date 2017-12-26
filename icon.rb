9a6d6d12e927bbc1ab6dc3e60bc59667ca6f53d3
Yota Toyama <raviqqe@gmail.com>
2017-12-26 13:42:44 +0900

    Format icon.rb

--- icon.rb
+++ icon.rb
@@ -7,8 +7,7 @@ SECONDARY = 'tomato'.freeze
 
 puts(xml do
   svg xmlns: 'http://www.w3.org/2000/svg', width: 160, height: 160 do
-    path d: 'M 0 0 h 160 l -80 80 l 80 80 h -160',
-         fill: PRIMARY
+    path d: 'M 0 0 h 160 l -80 80 l 80 80 h -160', fill: PRIMARY
     rect x: 30, y: 30, width: 100, height: 100, fill: SECONDARY
     rect x: 60, y: 60, width: 40, height: 40, fill: PRIMARY
   end
