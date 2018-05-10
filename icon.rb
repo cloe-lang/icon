ff34935c6c73547026af66194f34b0cd5ebf0e08
Yota Toyama <raviqqe@gmail.com>
2018-05-10 12:22:16 +0900

    Set viewBox instead of width and height

--- icon.rb
+++ icon.rb
@@ -6,7 +6,7 @@ PRIMARY = 'teal'.freeze
 SECONDARY = 'tomato'.freeze
 
 puts(xml do
-  svg xmlns: 'http://www.w3.org/2000/svg', width: 160, height: 160 do
+  svg xmlns: 'http://www.w3.org/2000/svg', viewBox: '0 0 160 160' do
     path d: 'M 0 0 h 160 l -80 80 l 80 80 h -160', fill: PRIMARY
     rect x: 30, y: 30, width: 100, height: 100, fill: SECONDARY
     rect x: 60, y: 60, width: 40, height: 40, fill: PRIMARY
